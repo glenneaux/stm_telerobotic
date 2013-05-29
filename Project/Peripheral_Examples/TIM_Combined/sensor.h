c87d77edd0a97cdbce05fbf64ea8fc43175b472b
glenneaux <glenneaux@opdev-sony.(none)>
2013-05-29 21:35:05 +1000

    Modularize

---
+++ Project/Peripheral_Examples/TIM_Combined/sensor.h
@@ -0,0 +1,11 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SENSOR_H
+#define __SENSOR_H
+
+#include <main.h>
+
+void Demo_CompassConfig(void)
+void Demo_CompassReadAcc(float* pfData)
+
+
+#endif __SENSOR_H
